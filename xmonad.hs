8152fd32d144ae9646f659ba55036893621875c2
Ben <swansontb@gmail.com>
2017-02-19 04:16:47 -0800

    Revamp to fix werid xmobar /toggle struts issues.
    Not quite sure what broke but this fixed it.

--- xmonad.hs
+++ xmonad.hs
@@ -1,51 +1,45 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.EZConfig(additionalKeysP)
+import XMonad.Layout.NoBorders(smartBorders)
+import XMonad.Hooks.ManageHelpers
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig
-import XMonad.Layout.SimplestFloat
-import XMonad.Layout.ThreeColumns
-import XMonad.Layout.NoBorders
 import System.IO
-import XMonad.Hooks.EwmhDesktops
 
-myWorkspaces = ["web", "term", "edit", "ssh", "music", "extra", "steam"] ++ map show [8..9]
-myManageHook = composeAll . concat $
-               [ [ className =? c --> doFloat | c <- cFloats ]
-               , [ title =? t --> doFloat | t <- tFloats ]
-	       , [className =? "Steam" --> doShift "steam"]
+myShiftHook = composeAll . concat $
+               [ [className =? "Steam" --> doShift "steam"]
+	       , [className =? "Google Play Music Desktop Player" --> doShift "music"]
 	       , [className =? "Firefox" --> doShift "web"]]
-  where cFloats = ["NES"]
-        tFloats = ["Firefox Preferences", "Downloads", "Add-ons", "Rename", "Create" ]
-myLayout = avoidStruts (
-    ThreeColMid 1 (3/100) (1/2) |||
-    Tall 1 (3/100) (1/2) |||
-    Mirror (Tall 1 (3/100) (1/2)) |||
-    Full)
-main = do
-     xmproc <- spawnPipe "/usr/bin/xmobar"
-     xmonad $ defaultConfig
-     	    	    { borderWidth = 2
-	    	    , terminal 	= "xterm -rv"
-                    , workspaces = myWorkspaces
-		    , normalBorderColor = "#cccccc"
-		    , focusedBorderColor = "#112255"
-		    , focusFollowsMouse = False
-		    , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
-		    , manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig
-		    , layoutHook = smartBorders $ myLayout
-		    , logHook = dynamicLogWithPP xmobarPP
-		      { ppOutput = hPutStrLn xmproc
-		      ,	ppTitle = xmobarColor "green" "" . shorten 50
-		      , ppLayout = const "" -- to disable the layout info on xmobar
-		      }
-		    , modMask = mod4Mask}
-		    `additionalKeysP`
-		    [("M-f", spawn "firefox")
-		    ,("M-e", spawn "emacs")
-		    ,("<XF86AudioRaiseVolume>", spawn  "amixer -q set Master 5+")
-                    ,("<XF86AudioLowerVolume>", spawn  "amixer -q set Master 5-")
-                    ,("<XF86AudioMute>", spawn  "amixer -q set Master toggle")
+
+myConfig = defaultConfig
+	{ manageHook = ( isFullscreen --> doFullFloat ) <+> myShiftHook <+> manageDocks <+> manageHook defaultConfig 
+	, layoutHook = smartBorders (avoidStruts $  layoutHook defaultConfig)
+	, terminal = "xterm -rv"
+	, borderWidth = 2
+        , focusFollowsMouse = False
+	, focusedBorderColor = "#112255"
+        , workspaces = ["web", "term", "edit", "ssh", "music", "extra", "steam"] ++ map show [8..9]
+	, modMask = mod4Mask
+	} `additionalKeysP`
+	[("M-f", spawn "firefox")
+		    ,("M-c", spawn "chromium")
+		    ,("<XF86AudioRaiseVolume>", spawn  "amixer -q set Master 2+")
+                    ,("<XF86AudioLowerVolume>", spawn  "amixer -q set Master 2-")
+                    ,("<XF86AudioMute>", spawn  "pactl set-sink-mute 0 toggle")
 		    ,("M-S-=", spawn  "sudo pm-suspend")
-		    ,("M-s", sendMessage ToggleStruts)
+		    ,("M-S-0", spawn  "sudo pm-hibernate")
+		    ,("M-S--", spawn  "sleep 1; xset s activate")
 		    ,("M-S-l", spawn  "xscreensaver-command --lock")]
+
+-- The main function.
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+-- Incase I need to launch something other than xmobar
+myBar = "xmobar"
+
+-- What is written to xmobar
+myPP = xmobarPP { ppCurrent = xmobarColor "green" "" . wrap "<" ">" . shorten 68
+		 , ppLayout = const "" }
+
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_s)
